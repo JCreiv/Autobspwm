0d7f783f842465b74719537a8625bb4726b7f706
JCreiv <115183412+JCreiv@users.noreply.github.com>
2025-05-20 23:07:57 +0200

    Update .fzf.zsh

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,7 +1,7 @@
 # Setup fzf
 # ---------
 if [[ ! "$PATH" == */home/javi/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/javi/.fzf/bin"
+  PATH="${PATH:+${PATH}:}/home/$USER/.fzf/bin"
 fi
 
 source <(fzf --zsh)
