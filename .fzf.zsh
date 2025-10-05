b3eef4a6b3123f56f78e52476dd63ea63d06cb37
JCreiv <115183412+JCreiv@users.noreply.github.com>
2025-10-06 00:36:19 +0200

    Update .fzf.zsh

--- .fzf.zsh
+++ .fzf.zsh
@@ -1,7 +1,19 @@
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == */home/$USER/.fzf/bin* ]]; then
-  PATH="${PATH:+${PATH}:}/home/$USER/.fzf/bin"
+
+# Detectar ubicación de fzf según el usuario
+if [ "$USER" = "root" ]; then
+  FZF_PATH="/home/javi/.fzf/bin"
+else
+  FZF_PATH="$HOME/.fzf/bin"
 fi
 
-source <(fzf --zsh)
+# Añadir al PATH si no está ya
+if [[ ! "$PATH" == *"$FZF_PATH"* ]]; then
+  PATH="${PATH:+${PATH}:}$FZF_PATH"
+fi
+
+# Cargar keybindings y completado usando ruta absoluta
+if [ -x "$FZF_PATH/fzf" ]; then
+  source <("$FZF_PATH/fzf" --zsh)
+fi
